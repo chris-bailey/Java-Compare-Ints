9612239a7b8ef77f19323111f8ea7202d6ca9c07
chris-bailey <chris@chrisbaileydeveloper.com>
2015-03-30 16:09:04 -0400

    Formatting changes so that code fits properly on website.

--- src/main/java/com/chrisbaileydeveloper/javacompareints/Main.java
+++ src/main/java/com/chrisbaileydeveloper/javacompareints/Main.java
@@ -14,24 +14,30 @@ public class Main {
 		int x = 5;
 		int y = 5;
 
-		System.out.println("Are the two integers equal?  " + compareInts(x, y));
+		System.out.print("Are the two integers equal? ");
+		System.out.print(compareInts(x, y));
 	}
 
 	/**
-	 * Returns true if the two integers given are equal. Returns false otherwise.
+	 * Returns true if the two integers given are equal. 
+	 * Returns false otherwise.
 	 * 
 	 * @param x the first integer value
 	 * @param y the second integer value
 	 * @return true if the two integers are equal, false otherwise
 	 */
 	public static boolean compareInts(int x, int y) {
-		int z = (x - y) | (y - x); // If unequal then z will be negative.
+		// If unequal then z will be negative.
+		int z = (x - y) | (y - x); 
 
-		z = z >> 31; // All 0's in binary if equal. All 1's if unequal.
+		// All 0's in binary if equal. All 1's if unequal.
+		z = z >> 31; 
 
-		z = ~z; // All 1's in binary if equal. All 0's if unequal.
+		// All 1's in binary if equal. All 0's if unequal.
+		z = ~z; 
 
-		// Use a map to return a boolean value without comparison operators
+		// Use a map to return a boolean value 
+		// without using comparison operators
 		Map<Integer, Boolean> map = new HashMap<>();
 		map.put(-1, true);
 		map.put(0, false);
--- src/test/java/com/chrisbaileydeveloper/javacompareints/MainTest.java
+++ src/test/java/com/chrisbaileydeveloper/javacompareints/MainTest.java
@@ -2,9 +2,7 @@ package com.chrisbaileydeveloper.javacompareints;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
-
 import com.chrisbaileydeveloper.javacompareints.Main;
 
 public class MainTest {
